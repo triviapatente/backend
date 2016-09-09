c924674ef37bec4b4081b2957b52d05ce165fd56
Luigi Donadel <luigi.donadel@gmail.com>
2016-09-09 15:08:00 +0200

    added configuration files and now run.py installs automatically dependencies

---
+++ dependencies.sh
@@ -0,0 +1,2 @@
+pip install flask-sqlalchemy
+pip install pytz
--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 # -*- coding: utf-8 -*-
+#!/usr/bin/python
+
+import sys
+
+#se lo script non è stato chiamato con la flag che forza l'avvio impedendo l'inizializzazione
+if len(sys.argv) < 2 or sys.argv[1] != "--f":
+    #esegui lo script di inizializzazione dell'app (che installa anche le dipendenze)
+    from subprocess import call
+    call(["sudo", "sh", "dependencies.sh"])
 
 from app import app
 
-app.run(host = '0.0.0.0', port = 8080, debug = True)
+app.run(host = '0.0.0.0', port = 8080, debug = app.config["DEBUG"])
