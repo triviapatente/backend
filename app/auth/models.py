74d166bfe7e21824856e5c285d902b455935a708
Luigi Donadel <luigi.donadel@gmail.com>
2016-09-13 11:24:30 +0200

    Fixed models configurations according to the documentation

--- app/auth/models.py
+++ app/auth/models.py
@@ -8,15 +8,22 @@ from app.base.models import Base, CommonPK
 from app.game.models import partecipation
 
 class User(Base, CommonPK):
+  #valori identificativi dell'utente, devono essere unici
   username = Column(String(250), nullable = False, unique = True)
   email = Column(String(250), nullable = False, unique = True)
+  #punteggio di partenza del giocatore
   score = Column(Integer, default = app.config["DEFAULT_USER_SCORE"])
+  #partite giocate dal giocatore
   games = relationship("Game", secondary = partecipation, back_populates = "users")
+  #partite vinte dal giocatore
   games_won = relationship("Game", back_populates = "winner")
 
 class Keychain(Base, CommonPK):
-
+  #utente che possiede il keychain
   user_id = Column(Integer, ForeignKey("user.id"), nullable = False)
   user = relationship("User")
-  password = Column(String) #dovrà essere hashata
+  #TODO: aggiungere controllo, uno dei due tra password o facebookToken deve essere sempre settato
+  #TODO: password dell'utente, dovrà essere salvata hashata
+  password = Column(String)
+  #facebookToken dell'utente
   facebookToken = Column(String)
--- app/game/models.py
+++ app/game/models.py
@@ -12,39 +12,54 @@ partecipation = Table('partecipation', Base.metadata,
 )
 
 class Game(Base, CommonPK):
+  #utenti che partecipano al gioco
   users = relationship("User", secondary = partecipation, back_populates = "games")
-  game_id = Column(Integer, ForeignKey("user.id"), nullable = False)
+  #utente vincitore
+  winner_id = Column(Integer, ForeignKey("user.id"))
   winner = relationship("User", back_populates = "games_won")
 
 #un round adesso rappresenta il round di un utente, e contiene tutte le info di quel round, come utente che sta giocando, categoria che ha scelto, game di appartenenza, domande proposte
 class Round(Base):
+  #numero del round, TODO: trovare il modo di farlo diventare autoincrement rispetto a diversi parametri
   number = Column(Integer, nullable = False)
+  #match di riferimento
   game_id = Column(Integer, ForeignKey("game.id"), nullable = False, primary_key = True)
   game = relationship("Game")
+  #utente che 'possiede' questo round
   user_id = Column(Integer, ForeignKey("user.id"), nullable = False, primary_key = True)
   user = relationship("User")
+  #categoria scelta per il round
   cat_id = Column(Integer, ForeignKey("category.id"), nullable = False)
   chosen_category = relationship("Category")
+  #domande chieste all'utente nel round
   questions = relationship("Question", back_populates = "round")
 
 class Quiz(Base, CommonPK):
-
+  #la domanda del quiz, in lettere
   question = Column(String(250), nullable = False)
+  #la risposta giusta, Vero/Falso
   answer = Column(Boolean, nullable = False)
+  #l'immagine di riferimento, se presente
   image_id = Column(Integer, ForeignKey("image.id"))
-  category_id = Column(Integer, ForeignKey("category.id"), nullable = False)
   image = relationship("Image")
+	#la categoria di riferimento (si punta sul fatto che ogni quiz sia categorizzato)
+  category_id = Column(Integer, ForeignKey("category.id"), nullable = False)
   category = relationship("Category")
 
 class Question(Base):
+  #quiz estratto
   quiz_id = Column(Integer, ForeignKey("quiz.id"), nullable = False, primary_key = True)
   quiz = relationship("Quiz")
+  #risposta data dall'utente
   answer = Column(Boolean, nullable = False)
+  #round di riferimento
   round_id = Column(Integer, ForeignKey("quiz.id"), nullable = False, primary_key = True)
   round = relationship("Round", back_populates = "questions")
 
 class Image(Base, CommonPK):
-  image = Column(String) #path dell'immagine, in alternativa: blob
+  #path dell'immagine, in alternativa: blob
+  image = Column(String)
 
 class Category(Base, CommonPK):
+  #nome della categoria
   name = Column(String(250), nullable = False)
