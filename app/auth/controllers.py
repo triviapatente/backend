2e2dd40d68cd5a90114919477fe96bfc4047d4c9
antonioterpin <antonio.terpin@gmail.com>
2016-09-25 17:41:09 +0200

    queries auth

--- app/auth/controllers.py
+++ app/auth/controllers.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import request, jsonify, Blueprint, g
 from app import app, db
+from app.auth.queries import *
 from app.auth.models import *
 from app.exceptions import *
 from app.decorators import auth_required, needs_post_values, needs_files_values
 from app.preferences.models import *
-from sqlalchemy import or_
 from app.utils import *
 import os
 
@@ -13,26 +13,20 @@ auth = Blueprint("auth", __name__, url_prefix = "/auth")
 account = Blueprint("account", __name__, url_prefix = "/account")
 info = Blueprint("info", __name__, url_prefix = "/info")
 
-@auth.route("/", methods = ["GET"])
-def welcome():
-    output = app.config["PUBLIC_INFOS"]
-    return jsonify(output)
-
 #api che effettua il login dell'utente
 @auth.route("/login", methods = ["POST"])
 #utilizzando il decorator che ho creato, posso fare il controllo dell'input in una riga
 @needs_post_values("user", "password")
 def login():
     #ottengo i valori in input
-    user_identifier = g.post.get("user")
     password = g.post.get("password")
     #ottengo l'user a partire dall'email o dall'username, e mi chiedo se c'è
-    user = User.query.filter(or_(User.email == user_identifier, User.username == user_identifier)).first()
+    user = getUserFromIdentifier(g.post.get("user"))
     if not user:
         #se no, login fallito!
         raise LoginFailed()
     #ottengo il keychain a partire dall'utente pescato dal db, e mi chiedo se c'è
-    keychain = Keychain.query.filter(Keychain.user_id == user.id).first()
+    keychain = getKeychain(user.id)
     if not keychain:
         #se no, login fallito! NB: questa cosa non dovrebbe mai accadere, vorrebbe dire che c'è un problema a livello di registrazione
         raise LoginFailed()
@@ -54,12 +48,12 @@ def register():
     username = g.post.get("username")
     email = g.post.get("email")
     password = g.post.get("password")
-    #vedo se ci sono altri utenti che hanno lo stesso username o la stessa password
-    u = User.query.filter((User.username == username or User.email == email)).first()
+    #vedo se ci sono altri utenti che hanno lo stesso username o la stessa email
+    u = getUserFromUsernameOrEmail(username, email)
     #se si, ti mando l'errore appropriato
     if u:
         raise AlreadyRegisteredUser(u, username, email)
-
+    return jsonify(user = u)
     #i controlli son passati, posso creare l'utente e salvarlo
     def createUser():
         db.session.autoflush = True
@@ -126,19 +120,10 @@ def changeImage():
     else:
         raise FormatNotAllowed()
 
+
 #api per la richiesta della classifica italiana (globale)
 @info.route("/rank/italy", methods = ["GET"])
 @auth_required
 def getItalianRank():
-    #vedo quante posizioni devo ritornare
-    n = app.config["RESULTS_LIMIT_RANK_ITALY"]
-    #chiedo la classifica (elenco utenti ordinato in base al punteggio) dei primi n utenti
-    rank = User.query.order_by(User.score).limit(n).all()
-    #filtro per punteggio maggiore di quello dell'utente e tra questi conto solo i punteggi distinti
-    userPosition = User.query.filter(User.score > g.user.score).distinct(User.score).count() + 1
-    #vedo se l'utente non è nelle prime 10 posizioni
-    if g.user not in rank:
-        #se non lo è, lo sostituisco all'ultima posizione (la decima)
-        rank[n - 1] = g.user
-    #ritorno la classifica e la posizione dell'utente
-    return jsonify(rank = rank, position = userPosition)
+    #chiedo la classifica (elenco utenti ordinato in base al punteggio) dei primi n utenti con le loro posizioni
+    return jsonify(rank = getRank())
