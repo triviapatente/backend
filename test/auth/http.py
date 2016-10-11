62cd13c4e3a2ae8abeca94c5aa83469151f7906b
antonioterpin <antonio.terpin@gmail.com>
2016-10-11 22:11:53 +0200

    test: logout, getCurrentUser, changeName, changeSurname

--- test/auth/http.py
+++ test/auth/http.py
@@ -15,7 +15,35 @@ class AuthHTTPTestCase(TPTestCase):
         response.json = json.loads(response.data)
         return response
 
+    def logout(self, token):
+        response = self.app.post("/auth/logout", headers = {"tp-session-token":token})
+        response.json = json.loads(response.data)
+        return response
+
+    def getCurrentUser(self, token):
+        response = self.app.get("/account/user", headers = {"tp-session-token":token})
+        response.json = json.loads(response.data)
+        return response
+
+    def changeName(self, token, name):
+        response = self.app.post("account/name/edit", data = {"name": name}, headers = {"tp-session-token":token})
+        response.json = json.loads(response.data)
+        return response
+
+    def changeSurname(self, token, surname):
+        response = self.app.post("account/surname/edit", data = {"surname": surname}, headers = {"tp-session-token":token})
+        response.json = json.loads(response.data)
+        return response
+
+    #TODO test change image
+
+    def getItalianRank(token):
+        response = self.app.get("/info/rank/italy", headers = {"tp-session-token":token})
+        response.json = json.loads(response.data)
+        return response
+
     ###TEST METHODS###
+    #per creare un metodo di test basta mettere test_ prima del metodo
 
     def test_register(self):
         print "#1: Registrazione corretta"
@@ -76,3 +104,45 @@ class AuthHTTPTestCase(TPTestCase):
         print "#4.1: password"
         response = self.login("user", None)
         assert response.status_code == 400
+
+    def test_getCurrentUser(self):
+        self.register("user", "user@gmail.com", "user")
+        token = self.login("user", "user").json.get("token")
+
+        print "#1: Accesso possibile con token valido"
+        response = self.getCurrentUser(token)
+        assert response.status_code == 200
+
+        print "#2: Accesso negato con token non valido"
+        response = self.getCurrentUser("INVALID_TOKEN")
+        assert response.status_code != 200
+
+    def test_logout(self):
+        self.register("user", "user@gmail.com", "user")
+        token = self.login("user", "user").json.get("token")
+
+        print "#1: Logout corretto"
+        response = self.logout(token)
+        assert response.status_code == 200
+
+        print "#2: Impossibile accedere con lo stesso token"
+        response = self.getCurrentUser(token)
+        assert response.status_code == 403
+
+    def test_changeName(self):
+        self.register("user", "user@gmail.com", "user")
+        token = self.login("user", "user").json.get("token")
+
+        print "#1: Cambio di nome effettuato"
+        response = self.changeName(token, "name")
+        assert response.status_code == 200 and response.json.get("user").get("name") == "name"
+
+    def test_changeSurname(self):
+        self.register("user", "user@gmail.com", "user")
+        token = self.login("user", "user").json.get("token")
+
+        print "#1: Cambio di cognome effettuato"
+        response = self.changeSurname(token, "surname")
+        assert response.status_code == 200 and response.json.get("user").get("surname") == "surname"
+
+    # TODO test get rank e change image
