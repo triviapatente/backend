d876808e96116c0ab94d20744dac9534bbdd8c6c
Luigi Donadel <luigi.donadel@gmail.com>
2016-10-12 12:35:19 +0200

    added main test structure. Now you can test both socket and http methods. Usage: put client methods on test/<module>/<type>/api.js, and test methods on test/<module><type>/tests.js, and import tests on test/pipeline.js. Now you are ready to test your application! (<type> = "socket" or "http")

--- test/shared.py
+++ test/shared.py
@@ -4,11 +4,17 @@ import os
 import unittest
 import tempfile
 from unittest import TestCase
-from app import app, db
+from app import app, db, socketio
 
 class TPTestCase(TestCase):
     def setUp(self):
         self.app = app.test_client()
+        self.socket = socketio.test_client(app)
         #il db è inizializzato a ogni test
         db.drop_all()
         db.create_all()
+
+class TPAuthTestCase(TPTestCase):
+    def setUp(self):
+        super.setUp()
+        #TODO: aggiungere autenticazione
