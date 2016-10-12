140ab46b8c6c3454224b8ae78537b539f2ea36f5
Luigi Donadel <luigi.donadel@gmail.com>
2016-10-12 13:18:14 +0200

    configured postgres with gitlab:ci method

--- scripts/init_ci.sh
+++ scripts/init_ci.sh
@@ -1,15 +1,4 @@
 apt-get -qqy update
-apt-get -qqy install postgresql python-psycopg2
+apt-get -qqy install python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
-
-#aggiungo l'utente ted nel sistema operativo
-adduser ted
-#lo creo a livello di postgres
-createuser -P -s -e ted
-#creo il db
-psql -c "CREATE DATABASE triviapatente"
-psql -c "CREATE DATABASE triviapatente_test "
-#do il privilegio all'utente ted
-psql -c "GRANT ALL PRIVILEGES ON DATABASE triviapatente TO ted"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE triviapatente_test TO ted"
