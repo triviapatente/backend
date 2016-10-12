aa4fda0350226ffd401eb5631657601e58fc8a62
Luigi Donadel <luigi.donadel@gmail.com>
2016-10-12 16:05:38 +0200

    fixed position of need_ci declaration

--- run_tests.py
+++ run_tests.py
@@ -7,15 +7,15 @@ if __name__ == '__main__':
     sys.setdefaultencoding('utf8')
     #importo il progetto e la inizializzo
     import tp
+    #controllo se si tratta di gitlab:ci
+    need_ci = len(sys.argv) >= 2 and sys.argv[1] == "-ci"
     tp.init(True, need_ci)
-    
+
     import unittest
 
     #creiamo le tabelle necessarie al testing
     from subprocess import call
     call(["sudo", "sh", "scripts/init_test.sh"])
-    #controllo se si tratta di gitlab:ci
-    need_ci = len(sys.argv) >= 2 and sys.argv[1] == "-ci"
     #elimino eventuali parametri aggiuntivi se presenti
     sys.argv = sys.argv[:1]
 
