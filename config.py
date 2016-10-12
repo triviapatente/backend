c0b8262eeb79e7599eaa60978428642582d2b426
Luigi Donadel <luigi.donadel@gmail.com>
2016-10-12 14:15:23 +0200

    added needed endpoint for ci

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 # Define the database - we are working with
 # SQLite for this example
 SQLALCHEMY_DATABASE_URI = 'postgresql://ted:ted@localhost:5432/triviapatente'
-SQLALCHEMY_TEST_DATABASE_URI = 'postgresql://ted:ted@localhost:5432/triviapatente_test'
+SQLALCHEMY_TEST_DATABASE_URI = 'postgresql://ted:ted@postgres:5432/triviapatente_test'
 
 # Application threads. A common general assumption is
 # using 2 per available processor cores - to handle
