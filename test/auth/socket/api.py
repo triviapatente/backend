b0f8828963b752fcfec05813020c98b735344001
antonioterpin <antonio.terpin@gmail.com>
2016-10-14 19:24:29 +0200

    fixed scalability and removed register in auth socket api

--- test/auth/socket/api.py
+++ test/auth/socket/api.py
@@ -1,7 +1,9 @@
 from test.auth.http.api import register as register_user
 from flask import json
 
-def login(self, token):
+def login(self, token = None):
+    if not token:
+        token = self.token
     self.socket.emit("auth", {"token": token})
     return self.socket.get_received()
 
