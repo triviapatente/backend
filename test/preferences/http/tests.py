10ddebaf332555f88f859de07c101a4f32646f55
antonioterpin <antonio.terpin@gmail.com>
2016-10-14 19:48:41 +0200

    add useful comments

--- test/preferences/http/tests.py
+++ test/preferences/http/tests.py
@@ -10,7 +10,6 @@ class PreferencesHTTPTestCase(TPAuthTestCase):
     #per creare un metodo di test basta mettere test_ prima del metodo
 
     def test_changeNotificationPreferences(self):
-
         # prendo tutte gli attributi con il ##notification_prefix
         notification_prefix = "notification_"
         notification_types = [notification_type[len(notification_prefix):] for notification_type in Preferences.__dict__.keys() if notification_prefix in notification_type]
@@ -24,8 +23,10 @@ class PreferencesHTTPTestCase(TPAuthTestCase):
     def test_changeStatsPreferences(self):
         url = "/preferences/stats/edit"
 
+        # prendo i possibili valori di stats
         enum_values = getEnumValues("stats")
         for i in range(0, len(enum_values)):
+            # provo a settare ciascun valore per stats
             print "#" + str(i+1) + ": Cambio preferenze statistiche in %s" % enum_values[i]
             response = changePreferences(self, url, enum_values[i])
             assert response.status_code == 200 and response.json.get("preferences").get("stats") == enum_values[i]
@@ -34,8 +35,10 @@ class PreferencesHTTPTestCase(TPAuthTestCase):
     def test_changeChatPreferences(self):
         url = "/preferences/chat/edit"
 
+        # prendo i possibili valori di chat
         enum_values = getEnumValues("chat")
         for i in range(0, len(enum_values)):
+            # provo a settare ciascun valore per chat
             print "#" + str(i+1) + ": Cambio preferenze chat in %s" % enum_values[i]
             response = changePreferences(self, url, enum_values[i])
             assert response.status_code == 200 and response.json.get("preferences").get("chat") == enum_values[i]
