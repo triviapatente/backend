b862cb082bc558ef263129fa6d8b4e693911e95c
antonioterpin <antonio.terpin@gmail.com>
2016-10-14 20:46:53 +0200

    change preferences scalability with self.token

--- test/preferences/http/api.py
+++ test/preferences/http/api.py
@@ -2,8 +2,10 @@
 
 from tp.preferences.models import Preferences
 
-def changePreferences(self, url, new_value):
-    return self.app.post(url, token = self.token, data = {"new_value": new_value})
+def changePreferences(self, url, new_value, token = None):
+    if not token:
+        token = self.token
+    return self.app.post(url, token = token, data = {"new_value": new_value})
 
 def getEnumValues(attr):
     return Preferences.__dict__[attr].property.columns[0].type.enums
