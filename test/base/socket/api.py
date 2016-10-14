d17837a56cf59580d8a01a0405c89be131edbc77
antonioterpin <antonio.terpin@gmail.com>
2016-10-14 17:24:38 +0200

    join and leave room scalability fixed

--- test/base/socket/api.py
+++ test/base/socket/api.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-def join_room(self, id, type):
-    self.socket.emit("join_room", {"id": id, "type": type});
-    return self.socket.get_received()
+def join_room(socket, id, type):
+    socket.emit("join_room", {"id": id, "type": type});
+    return socket.get_received()
 
-def leave_room(self, id, type):
-    self.socket.emit("leave_room", {"id": id, "type": type});
-    return self.socket.get_received()
+def leave_room(socket, id, type):
+    socket.emit("leave_room", {"id": id, "type": type});
+    return socket.get_received()
--- test/base/socket/tests.py
+++ test/base/socket/tests.py
@@ -20,60 +20,60 @@ class BaseSocketTestCase(TPAuthTestCase):
         game_id = self.game.get("id")
 
         print "#1 mi iscrivo a una room a cui posso entrare"
-        response = join_room(self, game_id, "game")
+        response = join_room(self.socket, game_id, "game")
         assert response.json.get("success") == True
 
         print "#2 mi iscrivo a una room a cui non posso entrare"
-        response = join_room(self, 324234, "game")
+        response = join_room(self.socket, 324234, "game")
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 403
 
         print "#3 mi iscrivo a una room di tipo non conosciuto"
-        response = join_room(self, game_id, "adfdsfsfd")
+        response = join_room(self.socket, game_id, "adfdsfsfd")
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 403
 
         print "#4 Parametri mancanti"
 
         print "#4.1 Manca room type"
-        response = join_room(self, game_id, None)
+        response = join_room(self.socket, game_id, None)
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 400
 
         print "#4.2 Manca room id"
-        response = join_room(self, None, "game")
+        response = join_room(self.socket, None, "game")
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 400
 
         print "#5 mi iscrivo a una room in cui son già entrato"
-        response = join_room(self, game_id, "game")
+        response = join_room(self.socket, game_id, "game")
         assert response.json.get("success") == True
 
     def test_leave_room(self):
         game_id = self.game.get("id")
 
         print "#1 mi tolgo da una room in cui sono presente"
-        join_room(self, game_id, "game")
-        response = leave_room(self, game_id, "game")
+        join_room(self.socket, game_id, "game")
+        response = leave_room(self.socket, game_id, "game")
         assert response.json.get("success") == True
 
         print "#2 mi tolgo da una room in cui non sono presente"
-        response = leave_room(self, game_id, "game")
+        response = leave_room(self.socket, game_id, "game")
         assert response.json.get("success") == True
 
         print "#3 mi tolgo da una room di tipo non conosciuto"
-        response = leave_room(self, game_id, "iojererj")
+        response = leave_room(self.socket, game_id, "iojererj")
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 403
 
         print "#4 Parametri mancanti"
 
         print "#4.1 Manca room type"
-        response = leave_room(self, game_id, None)
+        response = leave_room(self.socket, game_id, None)
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 400
 
         print "#4.2 Manca room id"
-        response = leave_room(self, None, "game")
+        response = leave_room(self.socket, None, "game")
         assert response.json.get("success") == False
         assert response.json.get("status_code") == 400
