0051d8800438c9a65fdd6e4df774ffe7ea12f057
Luigi Donadel <luigi.donadel@gmail.com>
2016-10-16 16:10:38 +0200

    fixed problem in getUsersFromRoom that prevented the method to return Users, because returned Partecipations

--- tp/base/utils.py
+++ tp/base/utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from flask import g
+from tp.auth.models import User
 from tp.game.models import Partecipation
 
 # enumeration of room type
@@ -26,9 +27,9 @@ def getInfosFromRoom(id):
 def getUsersFromRoom(type, game_id, check_for_user = False):
     #unico caso al momento, ma in caso di riutilizzo del sistema room ci saranno altri casi
     if type == RoomType.game.value:
-        query = Partecipation.query.filter(Partecipation.game_id == game_id)
+        query = User.query.join(Partecipation).filter(Partecipation.game_id == game_id)
         if check_for_user:
-            query.filter(Partecipation.user_id == g.user.id)
+            query.filter(User.id == g.user.id)
         return query.all()
     return None
 
