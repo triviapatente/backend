097269f55277b80a75950f5c8b79245848e4bff2
Luigi Donadel <luigi.donadel@gmail.com>
2016-11-11 12:03:54 +0100

    now on categories percentages query output there are also queries with percentage 0%. Changed kind of the output, now percentage is represented by total_questions and correct_questions and needs to be evaluated in the app

--- test/auth/socket/tests.py
+++ test/auth/socket/tests.py
@@ -1,15 +1,24 @@
 
 from test.shared import TPAuthTestCase
 from api import *
+from test.game.socket.utils import dumb_crawler
+from tp.game.models import Category
 
 class AuthSocketTestCase(TPAuthTestCase):
     def test_login(self):
+        dumb_crawler()
+
         print "#1: Successful login"
         response = login(self)
         assert response.json.get("invites") is not None
         assert response.json.get("global_rank_position") is not None
-        assert response.json.get("stats") is not None
         #TODO: assert response.json.get("friends_rank_position") is not None
+        assert response.json.get("stats") is not None
+
+        print "#2: Le statistiche sono su tutte le categorie"
+        stats = response.json.get("stats")
+        count = Category.query.count()
+        assert len(stats) == count
 
     def test_logout(self):
         response = login(self)
--- tp/stats/queries.py
+++ tp/stats/queries.py
@@ -2,7 +2,7 @@
 from tp import db
 from flask import g
 from tp.game.models import *
-from sqlalchemy import func, distinct
+from sqlalchemy import func, distinct, and_
 from sqlalchemy.orm import aliased
 from tp import db
 from datetime import datetime, timedelta
@@ -25,14 +25,9 @@ def getWrongLastQuestions(category_id):
 def getProgressChart(category_id, n, start, end = datetime.now()):
     time_slice = (timestamp(end) - timestamp(start)) / n
     delta = timedelta(seconds = time_slice)
-    print "offset:", start, "-", end,
-    print "time_slice:", time_slice, "seconds"
     output = {}
     cursor = start + delta
     while cursor <= end:
-        print "start", start
-        print "cursor", cursor
-        print "end", end
         output[str(cursor)] = getPercentageIn(category_id, start, cursor)
         start += delta
         cursor += delta
@@ -68,16 +63,15 @@ def getCategoryPercentages(user):
     #SELECT max(v."createdAt") AS max_1 FROM question, question AS b WHERE b.quiz_id = a.quiz_id
     max_created = db.session.query(b).with_entities(func.max(b.createdAt)).filter(b.quiz_id == a.quiz_id)
     #SELECT count(DISTINCT c.quiz_id) FROM question AS c JOIN quiz ON quiz.id = c.quiz_id WHERE quiz.category_id = category.id
-    total_questions = db.session.query(c).with_entities(func.count(distinct(c.quiz_id))).join(Quiz).filter(Quiz.category_id == Category.id)
+    total_questions = db.session.query(c).with_entities(func.count(distinct(c.quiz_id))).join(Quiz).filter(Quiz.category_id == Category.id).label("total_answers")
     #count(createdAt)
-    correct_questions = func.count(a.createdAt)
-    #SELECT category.id AS category_id, category.hint AS category_hint, (correct_questions * 100) / total_questions FROM category
-    #JOIN quiz ON quiz.category_id = category.id
-    #JOIN question ON quiz.id = question.quiz_id
-    #WHERE question.user_id = user_id
-    #AND question."createdAt" = max_created
-    #AND quiz.answer = question.answer
+    correct_questions = func.count(a.createdAt).label("correct_answers")
+    #SELECT category.id AS category_id, category.hint AS category_hint, correct_questions AS "correct_answers", total_questions  AS "total_answers"
+    #FROM quiz
+    #LEFT OUTER JOIN question AS a ON quiz.id = a.quiz_id AND quiz.answer = a.answer AND a."createdAt" = max_created AND a.user_id = 2
+    #JOIN category ON category.id = quiz.category_id
     #GROUP BY category.id, category.hint
-    query = db.session.query(a).join(Quiz).join(Category).with_entities(Category.id, Category.hint, (correct_questions * 100 / total_questions).label("percentage")).filter(a.user_id == user.id).filter(a.createdAt == max_created).filter(a.answer == Quiz.answer).group_by(Category.id, Category.hint)
+    #ORDER BY category.hint
+    query = db.session.query(Quiz).outerjoin(a, and_(a.quiz_id == Quiz.id, a.answer == Quiz.answer, a.createdAt == max_created, a.user_id == user.id)).join(Category).with_entities(Category.id, Category.hint, correct_questions, total_questions).order_by(Category.hint).group_by(Category.id, Category.hint)
     output = query.all()
     return output
