e36fa1f57c0605a5b59102e73c26fb13dbfc4a98
antonioterpin <antonio.terpin@gmail.com>
2017-02-02 21:08:14 +0100

    add general on category percentages

--- tp/stats/queries.py
+++ tp/stats/queries.py
@@ -114,5 +114,5 @@ def getCategoryPercentages(user):
     categoryPercentages = []
     for (id, hint, correct_answers, total_answers) in output:
         categoryPercentages.append({'id': id, 'hint': hint, 'correct_answers': correct_answers, 'total_answers': total_answers})
-    output.insert(0, general)
+    categoryPercentages.insert(0, general)
     return categoryPercentages
