23f0b3887eecfd18ecf6161947fda31bfc5467ce
Luigi Donadel <luigi.donadel@gmail.com>
2017-10-24 10:27:57 +0200

    added confirm password check and other fixes

---
+++ tp/static/js/change_password.js
@@ -0,0 +1,13 @@
+$(checkPasswords);
+function checkPasswords() {
+    $("#password, #confirm-password").keyup(checkPasswordInput);
+}
+
+function checkPasswordInput() {
+    var password = $("#password").val();
+    var confirmPassword = $("#confirm-password").val();
+    if(password == confirmPassword)
+        $("#submit").removeAttr("disabled");
+    else
+        $("#submit").attr("disabled", true);
+}
