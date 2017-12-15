7a9fe562582c2f76cbdf52b29aaf977d0671211d
Luigi Donadel <luigi.donadel@gmail.com>
2017-12-15 22:12:52 +0100

    Configured ssl

--- run.py
+++ run.py
@@ -20,8 +20,14 @@ from tp import app, socketio
 
 debug = app.config["DEBUG"]
 port = app.config["PORT"]
+context = None
+if debug == False:
+    from OpenSSL import SSL
+    context = SSL.Context(SSL.SSLv23_METHOD)
+    context.use_privatekey_file('/etc/ssl/tp_certs/www.triviapatente.it_private_key.key')
+    context.use_certificate_file('/etc/ssl/tp_certs/www.triviapatente.it_ssl_certificate.cer')
 
 print 'Running the service..'
-socketio.run(app, host = '0.0.0.0', port = port, debug = debug)
+socketio.run(app, host = '0.0.0.0', port = port, debug = debug, ssl_context=context)
 
 call(["fuser", "-k", "%d/tcp" % port])
