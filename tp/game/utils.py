38ea6bfb3cdd1bdbad86955c45defcaf4b9d56a0
Luigi Donadel <luigi.donadel@gmail.com>
2017-12-17 10:58:49 +0100

    Revert "trying to fix push problem"
    This reverts commit 5610dd78ee760888baf830651fa3d351b699b83e.

--- tp/game/utils.py
+++ tp/game/utils.py
@@ -394,8 +394,7 @@ def getNextRoundNumber(game):
     return getNextRoundNumberFor(game, g.user)
 
 def getOpponentFrom(game):
-    partecipation = Partecipation.query.filter(Partecipation.game_id == game.id).filter(Partecipation.user_id != g.user.id).first()
-    return User.get(partecipation.user_id)
+    return User.query.join(Partecipation).filter(Partecipation.game_id == game.id).filter(Partecipation.user_id != g.user.id).first()
 
 def isOpponentTurn(game):
     opponent = getOpponentFrom(game)
