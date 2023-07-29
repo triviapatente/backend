d09b1000b0a802241cdc782d288c0f3bf2687582
Antonio Terpin <antonio.terpin@gmail.com>
2023-07-29 19:39:16 +0200

    Fix query

--- test/base/socket/api.py
+++ test/base/socket/api.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 def join_room(socket, id, type, token):
-    socket.emit("join_room", {"tp-session-token": token, "body": {"id": id, "type": type}});
+    socket.emit("join_room", {"tp-session-token": token, "body": {"id": id, "type": type}})
     return socket.get_received()
 
 def leave_rooms(socket, type, token):
-    socket.emit("leave_room", {"tp-session-token": token, "body": {"type": type}});
+    socket.emit("leave_room", {"tp-session-token": token, "body": {"type": type}})
     return socket.get_received()
 
 def global_infos(socket, token):
